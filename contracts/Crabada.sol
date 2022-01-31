3c8c41c5ca6332748a486636bfbfd02391799e7a
paul4912 <paulyang4912@gmail.com>
2022-02-01 10:44:35 +1100

    add crabada interface

---
+++ contracts/Crabada.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+interface Crabada {
+    function startGame(uint256 teamId) external;
+
+    function closeGame(uint256 gameId) external;
+}
--- scripts/Mining.ts
+++ scripts/Mining.ts
@@ -1,7 +1,52 @@
 import { ethers } from "hardhat"
+import axios from "axios"
+
+const contractAbi = JSON.parse(`[
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "teamId",
+                "type": "uint256"
+            }
+        ],
+        "name": "startGame",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "gameId",
+                "type": "uint256"
+            }
+        ],
+        "name": "closeGame",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]`);
+
 
 async function main() {
+    const mineAddress = "0x82a85407BD612f52577909F4A58bfC6873f14DA8"
+    const contract = new ethers.Contract(mineAddress, contractAbi)
 
+    console.log("YPO")
+    await axios.get('https://idle-api.crabada.com/public/idle/mines?user_address=0x4f99949cc732f6c19ca58bd4fc750380bc51b76c&page=1&status=open&limit=8')
+        .then(response => {
+            console.log("YPO")
+            console.log(response.data.result);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    //const mineTrans = await contract.startGame('729')
+    //mineTrans.wait()
 }
 
 main()
