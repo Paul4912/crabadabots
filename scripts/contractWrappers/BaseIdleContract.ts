4879cc7a4389027565e8c0a1a47465420c0dfa88
paul4912 <paulyang4912@gmail.com>
2022-03-01 21:30:07 +1100

    logging price format ethers from wei

--- scripts/contractWrappers/BaseIdleContract.ts
+++ scripts/contractWrappers/BaseIdleContract.ts
@@ -58,7 +58,7 @@ abstract class BaseIdleContract {
 
     await TimeHelper.apiTimeout(this.reinforce(mine.game_id, reinforceCrab), 300000);
 
-    Logger.Log(LogAction.Success, `Reinforced with crab id ${reinforceCrab.crabada_id}. BP: ${reinforceCrab.battle_point} MP: ${reinforceCrab.mine_point} Price: ${reinforceCrab.price}.`)
+    Logger.Log(LogAction.Success, `Reinforced with crab id ${reinforceCrab.crabada_id}. BP: ${reinforceCrab.battle_point} MP: ${reinforceCrab.mine_point} Price: ${ethers.utils.formatEther(reinforceCrab.price)}.`)
   }
 
   public async sleepWhileGasPriceIsTooHigh(gasPriceLimit: number | undefined = undefined) {
